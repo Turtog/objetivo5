093ba6abaaf41a1996ed9850f695a734c50f4d4d
Turtog <joaonunes.pl018@academico.ifsul.edu.br>
2023-12-01 09:53:32 -0300

    first commit

---
+++ src/test/java/br/edu/ifsul/cstsi/objetivo5/Objetivo5ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.cstsi.objetivo5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Objetivo5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
